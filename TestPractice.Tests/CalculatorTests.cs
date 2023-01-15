8d1dee9343cfbab678a9da6e22ce0f4172814c00
Анна Башкатова <annabashk13@gmail.com>
2023-01-15 13:53:43 +0300

    Написание юнит тестов

---
+++ TestPractice.Tests/CalculatorTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPractice.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Additional(100, 200) == 300);
+        }
+
+        [Test]
+        public void SubtractionTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Subtraction(100, 30) == 70);
+        }
+
+        [Test]
+        public void MiltiplicationTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(30, 4) == 120);
+        }
+
+        [Test]
+        public void DivisionTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Division(100, 20) == 5);
+        }
+
+    }
+}
--- TestPractice/Program.cs
+++ TestPractice/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    public class Calculator
+    {
+        public int Additional(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Miltiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
+
